17195b809ac43730dafc717e5e59b08f5452b283
Dmitrij Zherebjarjew <zherebjatjew@gmail.com>
2013-05-31 21:03:18 +0700

    Hello world

---
+++ src/com/example/antispam/SmsReceiver.java
@@ -0,0 +1,11 @@
+package com.example.antispam;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dj
+ * Date: 31.05.13
+ * Time: 21:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class SmsReceiver {
+}
