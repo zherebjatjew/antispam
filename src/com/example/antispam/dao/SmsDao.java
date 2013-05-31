b5a1cbc19270eeacf0095db8edabf137625d9ec3
Dmitrij Zherebjarjew <zherebjatjew@gmail.com>
2013-05-31 22:17:52 +0700

    Add dao skeleton

---
+++ src/com/example/antispam/dao/SmsDao.java
@@ -0,0 +1,11 @@
+package com.example.antispam.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dj
+ * Date: 31.05.13
+ * Time: 22:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class SmsDao {
+}
