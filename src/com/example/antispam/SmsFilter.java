6b4cb23ff21ecd5b5cd2587808aea595f0128a1a
Dmitrij Zherebjarjew <zherebjatjew@gmail.com>
2013-05-31 22:08:11 +0700

    Intercept incoming SMS messages

--- src/com/example/antispam/SmsFilter.java
+++ src/com/example/antispam/SmsFilter.java
@@ -1,5 +1,7 @@
 package com.example.antispam;
 
+import android.telephony.SmsMessage;
+
 /**
  * Created with IntelliJ IDEA.
  * User: dj
@@ -8,4 +10,23 @@ package com.example.antispam;
  * To change this template use File | Settings | File Templates.
  */
 public class SmsFilter {
+	public boolean isUnwelcome(SmsMessage message) {
+		String from = message.getOriginatingAddress();
+		if (isFromWhiteList(from)) return false;
+		if (isFromGateway(from)) return true;
+		if (isFromBlackList(from)) return true;
+		return false;
+	}
+
+	private boolean isFromBlackList(String sender) {
+		return false;
+	}
+
+	private boolean isFromWhiteList(String sender) {
+		return false;
+	}
+
+	private boolean isFromGateway(String sender) {
+		return false;
+	}
 }
--- src/com/example/antispam/SmsReceiver.java
+++ src/com/example/antispam/SmsReceiver.java
@@ -1,5 +1,14 @@
 package com.example.antispam;
 
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
 /**
  * Created with IntelliJ IDEA.
  * User: dj
@@ -7,5 +16,39 @@ package com.example.antispam;
  * Time: 21:17
  * To change this template use File | Settings | File Templates.
  */
-public class SmsReceiver {
+public class SmsReceiver extends BroadcastReceiver {
+	private static final String TAG = SmsReceiver.class.getSimpleName();
+	private static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
+
+	private SmsFilter filter = new SmsFilter();
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (intent != null && ACTION.equals(intent.getAction())) {
+			SmsMessage smsMessage = extractSmsMessage(intent);
+			processMessage(context, smsMessage);
+		}
+	}
+
+	private SmsMessage extractSmsMessage(Intent intent) {
+		Bundle pudsBundle = intent.getExtras();
+		Object[] pdus = (Object[]) pudsBundle.get("pdus");
+		SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdus[0]);
+		return smsMessage;
+	}
+
+	private void processMessage(Context context, SmsMessage smsMessage) {
+		Log.i(TAG, "Received SMS from " + smsMessage.getOriginatingAddress());
+		// Do some stuff here
+		if (filter.isUnwelcome(smsMessage)) {
+			Log.i(TAG, "SMS rejected due to spam");
+			archiveMessage(smsMessage);
+			abortBroadcast();
+		}
+	}
+
+	private void archiveMessage(SmsMessage smsMessage) {
+		//To change body of created methods use File | Settings | File Templates.
+	}
+
 }
